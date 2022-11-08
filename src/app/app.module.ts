cfcc956de36ed87e20696f794a64cb7f0185ff50
Maia-Aedo <aedomaia10@gmail.com>
2022-11-07 23:31:30 -0300

    navbar y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    EquipoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
